f14a658da28450dd65a8ee6a74ca845e10e428e0
AndrewAnggaraPuteraWisesarpl5 <andrew_wisesa_30rpl@student.smktelkom-mlg.sch.id>
2024-05-18 11:48:54 +0700

    maskapai dan hotel

--- cdn_arrosyid/__manifest__.py
+++ cdn_arrosyid/__manifest__.py
@@ -17,16 +17,19 @@
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '16.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/menu.xml',
+        'views/maskapai.xml',
+        'views/hotel.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
