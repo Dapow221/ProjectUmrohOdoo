71352ff5303d7a94514293d2efd4424c4ad13c81
Gifari Kemal <gifariik@gmail.com>
2024-05-19 19:40:46 +0700

    penambahan field dari model res.partner yang diinherit

--- cdn_arrosyid/models/identitas_jamaah.py
+++ cdn_arrosyid/models/identitas_jamaah.py
@@ -2,7 +2,7 @@ from odoo import models, fields, api
 
 
 class IdentitasJamaah(models.Model):
-    _name            = 'cdn.identitas.jamaah'
+    _name            = 'cdn.identitas.jamaah' 
     _description     = 'Identitas Jamaah'
     _inherits        = {'res.partner':'partner_id'}
 
@@ -11,13 +11,13 @@ class IdentitasJamaah(models.Model):
     jenis_kel        = fields.Selection(string='Jenis Kelamin', selection=[('l', 'Laki-laki'), ('p', 'Perempuan'),], default='l')
     paspor           = fields.Integer(string='Nomor Paspor')
     masa_paspor      = fields.Date(string='Masa berlaku Paspor')
-    alamat           = fields.Text(string='Alamat')
+    # alamat           = fields.Text(string='Alamat') #ambil dari res.partner saja
+    # no_hp = fields.Integer(string='Nomor Handphone') #ambil dari res.partner saja
     tgl_lahir        = fields.Date(string='Tanggal Lahir')
     umur             = fields.Integer(string='umur') #compute tgllahir
     is_menikah = fields.Boolean(string='Sudah menikah?', default = False)
     nama_pasangan    = fields.Char(string='Nama Pasangan')
-    status_petugas   = fields.Selection(string='Status Petugas', selection=[('siap', 'Siap'), ('sibuk', 'Sibuk'),])
+    status_petugas   = fields.Selection(string='Status Petugas', selection=[('siap', 'Siap'), ('sibuk', 'Sibuk'),], default ='siap')
     riwayat_penyakit = fields.Char(string='Riwayat Penyakit')
-    
     
     
