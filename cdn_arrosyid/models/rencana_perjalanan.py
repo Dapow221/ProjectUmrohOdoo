b17557a7933752ce27fbf15ebde1c6fb9e80e340
will2308 <apelijo02@gmail.com>
2024-05-20 11:12:22 +0700

    add rec name in cdn.rencana.perjalanan

--- cdn_arrosyid/models/rencana_perjalanan.py
+++ cdn_arrosyid/models/rencana_perjalanan.py
@@ -5,6 +5,7 @@ from odoo import models, fields, api
 class CdnRencanaPerjalanan(models.Model):
     _name = 'cdn.rencana.perjalanan'
     _description = 'Cdn Rencana Perjalanan'
+    _rec_name = 'nama'
 
     sesi_umroh_id = fields.Many2one('cdn.sesi.umroh', string='sesi_umroh')
     nama = fields.Char(string='Nama')
