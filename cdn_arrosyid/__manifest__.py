d73950640a6548f943dcae3124cd03b856bf7454
Dapow221 <141142350+Dapow221@users.noreply.github.com>
2024-05-20 16:26:12 +0700

    adding invoice

--- cdn_arrosyid/__manifest__.py
+++ cdn_arrosyid/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '1.0.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'product'],
+    'depends': ['base', 'product', 'account'],
 
     # always loaded
     'data': [
