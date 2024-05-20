24766c2d9fad0012112e7863a7ae490d1624d989
AndrewAnggaraPuteraWisesarpl5 <andrew_wisesa_30rpl@student.smktelkom-mlg.sch.id>
2024-05-20 09:22:02 +0700

    solve_field

--- cdn_arrosyid/models/hotel.py
+++ cdn_arrosyid/models/hotel.py
@@ -5,9 +5,9 @@ from odoo import models, fields, api
 class CdnHotel(models.Model):
     _name = 'cdn.hotel'
     _description = 'Hotel'
-    _inherits={'res.partner':'partner_id'}
+    _inherits={'res.company':'company_id'}
 
-    partner_id = fields.Many2one(comodel_name='res.partner', string='Hotel')
+    company_id = fields.Many2one(comodel_name='res.company', string='Hotel')
     type = fields.Selection(string='Type Hotel', selection=[('bintang_1', 'Bintang 1'), ('bintang_2', 'Bintang 2'), 
     ('bintang_3', 'Bintang 3'), ('bintang_4', 'Bintang 4'), ('bintang_5', 'Bintang 5')])
     keterangan = fields.Text(string='Keterangan')  
--- cdn_arrosyid/models/maskapai.py
+++ cdn_arrosyid/models/maskapai.py
@@ -3,11 +3,11 @@ from odoo import models, fields, api
 class CdnMaskapai(models.Model):
     _name = 'cdn.maskapai'
     _description = 'Maskapai'
-    _inherits={'res.partner':'partner_id'}
+    _inherits={'res.company':'company_id'}
 
-    partner_id = fields.Many2one(comodel_name='res.partner', string='Maskapai')
+    company_id = fields.Many2one(comodel_name='res.company', string='Maskapai')
     keterangan = fields.Text(string='Keterangan')
-    # sesi_id = fields.Many2one(comodel_name='', string='Sesi Umroh')
+    sesi_id = fields.Many2one(comodel_name='', string='Sesi Umroh')
     harga = fields.Monetary('Harga', currency_field='currency_id')
     currency_id = fields.Many2one('res.currency', string='Currency')
     
