34a412c1961196fe6bec8c27dedaa965ecdefb42
Gifari Kemal <gifariik@gmail.com>
2024-05-20 12:53:17 +0700

    penambahan constrains no paspor dan masa paspor, komputasi umur, dan merapihkan form view

--- cdn_arrosyid/models/identitas_jamaah.py
+++ cdn_arrosyid/models/identitas_jamaah.py
@@ -1,5 +1,7 @@
-from odoo     import models, fields, api
-from datetime import date
+from odoo            import models, fields, api, _
+from datetime        import date
+from dateutil        import relativedelta
+from odoo.exceptions import ValidationError
 
 class IdentitasJamaah(models.Model):
     _name            = 'cdn.identitas.jamaah' 
@@ -10,14 +12,23 @@ class IdentitasJamaah(models.Model):
     kategori         = fields.Selection(string='Jenis Jamaah', selection=[('peserta', 'Peserta Umroh'), ('petugas_lap', 'Petugas Lapangan'),('penanggung_jawab', 'Penanggung Jawab')], default ='peserta')
     jenis_kel        = fields.Selection(string='Jenis Kelamin', selection=[('l', 'Laki-laki'), ('p', 'Perempuan'),], default='l')
     paspor           = fields.Char(string='Nomor Paspor', required=True)
-    masa_paspor      = fields.Date(string='Masa berlaku Paspor')
+    # referensi        = fields.Char(string='No Referensi')
+    masa_paspor      = fields.Date(string='Masa berlaku Paspor', required=True)
     tgl_lahir        = fields.Date(string='Tanggal Lahir')
-    umur             = fields.Integer(string='Umur', compute='_compute_umur') #compute tgllahir
+    umur             = fields.Integer(string='Umur', compute='_compute_umur') 
+    image            = fields.Image(string='image')
     is_menikah       = fields.Boolean(string='Sudah menikah?', default = False)
     nama_pasangan    = fields.Char(string='Nama Pasangan')
     status_petugas   = fields.Selection(string='Status Petugas', selection=[('siap', 'Siap'), ('sibuk', 'Sibuk'),], default ='siap')
     riwayat_penyakit = fields.Char(string='Riwayat Penyakit')
-
+    active           = fields.Boolean(string='Active', default= True)
+    
+    
+    # @api.model
+    # def create(self, vals):
+    #     if self.kategori == "peserta":
+    #         vals['referensi'] =self.env['ir.sequence'].next_by_code('cdn.identitas.jamaah.peserta')
+    #     return super(IdentitasJamaah, self).create(vals)
     
     @api.depends('tgl_lahir')
     def _compute_umur(self):
@@ -27,7 +38,17 @@ class IdentitasJamaah(models.Model):
                 rec.umur = today.year - rec.tgl_lahir.year
             else:
                 rec.umur = 1
-    
+                
     _sql_constraints = [
         ("paspor_unique", "unique(paspor)", "Nomor Paspor sudah tercatat"),
     ]
+    
+    @api.constrains('masa_paspor')
+    def _check_masa_paspor(self):
+        for rec in self:
+            today = date.today()
+            masa_berlaku_paspor = today - relativedelta.relativedelta(month=6)
+            if rec.masa_paspor and rec.masa_paspor < masa_berlaku_paspor:
+                raise ValidationError (_("Masa Berlaku Paspor harus lebih dari 6 bulan dari hari ini."))
+    
+    
