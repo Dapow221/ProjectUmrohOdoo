429cb104be14dbd5fbce0bbb54d740545b48723b
AndrewAnggaraPuteraWisesarpl5 <andrew_wisesa_30rpl@student.smktelkom-mlg.sch.id>
2024-05-24 09:15:03 +0700

    add field

--- cdn_arrosyid/models/res_partner.py
+++ cdn_arrosyid/models/res_partner.py
@@ -4,6 +4,6 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     jenis = fields.Selection(string='Jenis', selection=[('maskapai', 'Maskapai'), ('hotel', 'Hotel')])
-    kelas = fields.Selection(string='Bintang', selection=[('bintang_1', 'Bintang 1'), ('bintang_2', 'Bintang 2'), 
+    kelas = fields.Selection(string='Bintang', selection=[('bintang_0', 'Bintang 0'),('bintang_1', 'Bintang 1'), ('bintang_2', 'Bintang 2'), 
     ('bintang_3', 'Bintang 3'), ('bintang_4', 'Bintang 4'), ('bintang_5', 'Bintang 5'),], default='bintang_5')
     
