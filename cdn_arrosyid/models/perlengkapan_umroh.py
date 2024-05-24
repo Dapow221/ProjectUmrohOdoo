4fad341badaac041ab5daddf79aef1e1c49431e2
All <alliong75@gmail.com>
2024-05-24 11:27:59 +0700

    delete model perlengkapan

--- cdn_arrosyid/models/perlengkapan_umroh.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-
-class Perlengkapan(models.Model):
-    _inherit = 'product.product'
-
-    perlengkapan_umroh = fields.Selection(string='Jenis Perlengkapan Umroh', selection=[('konsumsi_umroh', 'Konsumsi Training'), ('peralatan_umroh', 'Perlatan Training')])
-    
