bce6aabc2d6647abb2d0c20e8cb5b39e82775f2b
All <alliong75@gmail.com>
2024-06-08 18:09:12 +0700

    fix perlengkapan & smart button jamaah

--- cdn_arrosyid/models/product_inherit.py
+++ cdn_arrosyid/models/product_inherit.py
@@ -6,5 +6,3 @@ class ProductProduct(models.Model):
     paket_umroh = fields.Boolean(string='Paket Umroh')
     paket_umroh_id = fields.Integer(string='')
     sesi_id = fields.Integer(string='')
-    
-    
