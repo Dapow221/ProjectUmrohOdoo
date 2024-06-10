18f77fb2980b8ab0c81fe96a9d64a21c6dbc6168
Dapow221 <141142350+Dapow221@users.noreply.github.com>
2024-06-10 16:43:21 +0700

    done

--- cdn_arrosyid/controllers/main.py
+++ cdn_arrosyid/controllers/main.py
@@ -7,7 +7,6 @@ class HomeController(http.Controller):
         return request.render('cdn_arrosyid.homepage')
         # return "Ini adalah halaman sesi umroh"
 
-<<<<<<< HEAD
     @http.route('/sesi_umroh', auth='public', website=True)
     def sesi_umroh(self, **kwargs):
             sesi_umroh_records = request.env['cdn.sesi.umroh'].sudo().search([])
@@ -15,45 +14,26 @@ class HomeController(http.Controller):
                 'sesi_umroh_records': sesi_umroh_records
             })
 
-    @http.route('/ketentuan', type='http', auth="public", website=True)
-    def get_ketentuan(self, **kw):
-=======
-
-        
-        return request.render('cdn_arrosyid.homepages')
-
     @http.route('/sesi', type='http', auth="public", website=True)
     def get_sesi(self, **kw):
 
-
-        
         return request.render('cdn_arrosyid.sesi_umroh')
 
     @http.route('/ketentuan', type='http', auth="public", website=True)
     def get_ketentuan(self, **kw):
-
-
         
->>>>>>> main
         return request.render('cdn_arrosyid.ketentuan_umum')
 
     @http.route('/pendaftaran', type='http', auth="public", website=True)
     def get_pendaftaran(self, **kw):
-<<<<<<< HEAD
         return request.render('cdn_arrosyid.pendaftaran_web')
 
     @http.route('/profil', type='http', auth="public", website=True)
     def get_profil(self, **kw):        
-=======
 
-
-        
         return request.render('cdn_arrosyid.pendaftaran_web')
 
     @http.route('/profil', type='http', auth="public", website=True)
     def get_profil(self, **kw):
-
-
         
->>>>>>> main
         return request.render('cdn_arrosyid.profil_web')
