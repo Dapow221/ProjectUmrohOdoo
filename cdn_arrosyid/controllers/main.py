b95612976714794537b29482ea18f6efe384d9e8
All <alliong75@gmail.com>
2024-06-10 16:14:08 +0700

    layout2

--- cdn_arrosyid/controllers/main.py
+++ cdn_arrosyid/controllers/main.py
@@ -2,57 +2,37 @@ from odoo import http
 from odoo.http import request
 
 class HomeController(http.Controller):
-    @http.route('/home', type='http', auth="public", website=True)
+    @http.route('/', type='http', auth="public", website=True)
     def get_home(self, **kw):
 
-        pendaftarans = request.env['cdn.pendaftaran'].sudo().search([])
 
-        data = {
-            'pendaftarans' : pendaftarans 
-        }
         
-        return request.render('cdn_arrosyid.homepage', data)
+        return request.render('cdn_arrosyid.homepages')
 
     @http.route('/sesi', type='http', auth="public", website=True)
     def get_sesi(self, **kw):
 
-        pendaftarans = request.env['cdn.pendaftaran'].sudo().search([])
 
-        data = {
-            'pendaftarans' : pendaftarans 
-        }
         
-        return request.render('cdn_arrosyid.sesi_umroh', data)
+        return request.render('cdn_arrosyid.sesi_umroh')
 
     @http.route('/ketentuan', type='http', auth="public", website=True)
     def get_ketentuan(self, **kw):
 
-        pendaftarans = request.env['cdn.pendaftaran'].sudo().search([])
 
-        data = {
-            'pendaftarans' : pendaftarans 
-        }
         
-        return request.render('cdn_arrosyid.ketentuan_umum', data)
+        return request.render('cdn_arrosyid.ketentuan_umum')
 
     @http.route('/pendaftaran', type='http', auth="public", website=True)
     def get_pendaftaran(self, **kw):
 
-        pendaftarans = request.env['cdn.pendaftaran'].sudo().search([])
 
-        data = {
-            'pendaftarans' : pendaftarans 
-        }
         
-        return request.render('cdn_arrosyid.pendaftaran_web', data)
+        return request.render('cdn_arrosyid.pendaftaran_web')
 
     @http.route('/profil', type='http', auth="public", website=True)
     def get_profil(self, **kw):
 
-        pendaftarans = request.env['cdn.pendaftaran'].sudo().search([])
 
-        data = {
-            'pendaftarans' : pendaftarans 
-        }
         
-        return request.render('cdn_arrosyid.profil_web', data)
+        return request.render('cdn_arrosyid.profil_web')
