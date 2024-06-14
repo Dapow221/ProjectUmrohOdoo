26725edc331426ae3f42e9eaf1dbafeeec511ac1
Gifari Kemal <gifariik@gmail.com>
2024-06-14 09:05:42 +0700

    Penambahan hotel dengan koordinat

--- cdn_arrosyid/models/res_partner.py
+++ cdn_arrosyid/models/res_partner.py
@@ -5,7 +5,7 @@ class ResPartner(models.Model):
 
     jenis = fields.Selection(string='Jenis', selection=[('maskapai', 'Maskapai'), ('hotel', 'Hotel')])
     kelas = fields.Selection(string='Bintang', selection=[('bintang_0', 'Bintang 0'),('bintang_1', 'Bintang 1'), ('bintang_2', 'Bintang 2'),('bintang_3', 'Bintang 3'), ('bintang_4', 'Bintang 4'), ('bintang_5', 'Bintang 5'),])
-    
-
-    
     lokasi_hotel = fields.Char(string='Lokasi Hotel')
+    latitude = fields.Float(string='latitude',  digits=(10, 8))
+    longitude = fields.Float(string='longitude', digits=(11, 8))
+    
