0e6832aef30c9991f4d28b5b3f157528ea4d9539
All <alliong75@gmail.com>
2024-06-17 01:02:05 +0700

    fix regis

--- cdn_arrosyid/controllers/signup.py
+++ cdn_arrosyid/controllers/signup.py
@@ -15,43 +15,27 @@ class SignupController(http.Controller):
             password = post.get('password')
             confirm_password = post.get('confirm_password')
 
-
-            # Cek apakah email sudah terdaftar
             partner_exists = request.env['res.partner'].sudo().search([('email', '=', email)])
             if partner_exists:
                 return json.dumps({'result': False, 'error': 'Email sudah terdaftar. Silakan gunakan email lain.'})
 
-            # Membuat partner baru jika belum terdaftar
             partner = request.env['res.partner']
-            partner_id = False
-            if not partner_exists:
-                partner_id = partner.create({
-                    'email': email,    
-                    'name': name,      
+            partner_id = partner.sudo().create({
+                'email': email,    
+                'name': name,      
+            })
+
+            request.env['res.users'].sudo().create({
+                'partner_id': partner_id.id,
+                'login': email,
+                'name': name,
+                'password': password,
+            })
+
+            request.env['cdn.identitas.jamaah'].sudo().create({
+                'partner_id': partner_id.id,
                 })
 
-            # Membuat user jika partner berhasil dibuat atau sudah ada
-            if partner_id or partner_exists:
-                user = request.env['res.users']
-                user.create({
-                    'partner_id': partner_id.id if partner_id else partner_exists.id,
-                    'login': email,
-                    'name': name,
-                    'password': password,
-                })
-
-                # Membuat objek cdn.identitas.jamaah jika partner berhasil dibuat atau sudah ada
-                partner_record = partner_id if partner_id else partner_exists
-                if partner_record:
-                    identitas_jamaah = request.env['cdn.identitas.jamaah'].create({
-                        'partner_id': partner_record.id,
-                        # Tambahkan atribut lain jika diperlukan
-                    })
-                    # Pastikan untuk melakukan commit setelah membuat cdn.identitas.jamaah
-                    request.env.cr.commit()
-
-            # Kembalikan respons JSON
             return json.dumps({'result': True})
         except Exception as e:
-            # Tangani kesalahan dan kembalikan pesan kesalahan
             return json.dumps({'result': False, 'error': str(e)})
